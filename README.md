f6d99caf325479e0b6083b947fbdbbc61a77a66e
Elizabeth Rapoport <elizrap@mit.edu>
2013-02-10 20:04:29 -0500

    Moved inner projectzero folder contents into outer one and deleted inner one

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ projectzero
 ===========
 
 6.170 project0
+Heroku App URL: http://thawing-hamlet-7541.herokuapp.com
---
+++ test/functional/sayhi_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SayhiControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
